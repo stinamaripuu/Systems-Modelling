90be691ceaec5c6d9d7475fbca5875ab1136fc4d
Kerdo Kurs <kerdokurs@gmail.com>
2024-09-19 12:02:14 +0300

    add initial project setup

---
+++ SysModProject/src/TestClass.java
@@ -0,0 +1,18 @@
+
+public class TestClass {
+private int test;
+
+public void setTest(int value) {
+   this.test = value;
+}
+
+public int getTest() {
+   return this.test;
+}
+
+public void test() {
+   // TODO implement this operation
+   throw new UnsupportedOperationException("not implemented");
+}
+
+}
